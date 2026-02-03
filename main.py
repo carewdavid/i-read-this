1675a295efa2b6f992e85457ef433474ac3d6071
David Carew <david@dcarew.com>
2026-02-03 11:11:55 -0800

    Add main form

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from i-read-this!")
+from flask import Flask, request, redirect, url_for
+
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    return """<title>I Read This</title>
+        <form method="POST" action="/add-article">
+        <label for="url">Article url:</label>
+        <input id="url" name="url" type="text">
+        <input type="submit" value="submit">
+        </form>"""
