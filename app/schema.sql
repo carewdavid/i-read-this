6a0e9540d589244a5c8cac65782ef9ec6f0b2036
David Carew <david@dcarew.com>
2026-02-05 20:47:29 -0800

    Update database schema to prepare for archiving links

--- app/schema.sql
+++ app/schema.sql
@@ -1,4 +1,11 @@
 create table if not exists links(
-id integer primary key autoincrement,
-url text not null,
-added_at default current_timestamp);
+	id integer primary key autoincrement,
+	url text not null,
+	added_at timestamp default current_timestamp,
+	is_live boolean);
+
+create table if not exists pages(
+	id integer PRIMARY KEY,
+	url text not null,
+	retrieved_at timestamp default current_timestamp,
+	content text);
